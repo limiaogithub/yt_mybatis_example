b1d877dced10800c29b768e75bde2613c0db16ce
limiao <18640290294@163.com>
2018-02-06 09:49:05 +0800

    2.3

--- src/main/java/com/github/yt/example/simple/service/impl/TestServiceImpl.java
+++ src/main/java/com/github/yt/example/simple/service/impl/TestServiceImpl.java
@@ -36,7 +36,7 @@ public class TestServiceImpl implements TestService {
         List<String> queryList = new ArrayList<>();
         queryList.add("18888888888");
         queryList.add("18888888889");
-        queryHandler.addWhereSql("t.age=#{data.age1} and t.phone in" + QueryHandler.getInSql("phone1", queryList.size()));
+        queryHandler.addWhereSql("t.age=#{data.age1} and t.phone in" + QueryHandler.getInSql("data.phone1", queryList.size()));
         queryHandler.addExpandData("age1", 30);
         queryHandler.addExpandData("phone1", queryList.toArray());
         List<MemberT> list = testMapper.findAll(new MemberT(), queryHandler);
